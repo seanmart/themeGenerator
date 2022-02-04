917ba72388cf5287d76e51211e914bbe22f07e8c
seanmart <sean@seanmart.com>
2022-02-04 16:45:57 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       {
         rel: "stylesheet",
         href:
-          "https://fonts.googleapis.com/css2?family=Satisfy&family=Work+Sans:wght@300;400;500;700;900&display=swap"
+          "https://fonts.googleapis.com/css2?family=Satisfy&family=Work+Sans:wght@300;500;700;900&display=swap"
       }
     ]
   },
